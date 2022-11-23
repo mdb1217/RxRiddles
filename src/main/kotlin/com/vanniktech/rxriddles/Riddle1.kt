4c58946c7f87d2066c248d18d9d1dd74c5d78626
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2022-11-23 22:32:33 +0900

    Solve : Riddle1

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle1.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle1.kt
@@ -3,12 +3,15 @@ package com.vanniktech.rxriddles
 import io.reactivex.rxjava3.core.Observable
 
 object Riddle1 {
-  /**
-   * Transform the given [value] into an Observable that emits the value and then completes.
-   *
-   * Use case: You want to transform some value to the reactive world.
-   */
-  fun solve(value: Int): Observable<Int> {
-    TODO()
-  }
+    /**
+     * Transform the given [value] into an Observable that emits the value and then completes.
+     *
+     * Use case: You want to transform some value to the reactive world.
+     */
+    fun solve(value: Int): Observable<Int> {
+        return Observable.create {
+            it.onNext(value)
+            it.onComplete()
+        }
+    }
 }
